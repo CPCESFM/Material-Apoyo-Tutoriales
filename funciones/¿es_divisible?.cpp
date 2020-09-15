3ce824c048cddf2c5e9556eb3b24572e1a6fe3d5
Algoritmia Esfm <cpcesfm@gmail.com>
2020-09-15 01:46:26 -0500

    ¿n divide a num?

---
+++ funciones/¿es_divisible?.cpp
@@ -0,0 +1,10 @@
+/*Devuelve true si num es divisible por n,
+evolución de esta función, obtener número
+de divisores de num*/
+
+bool es_divisible(int num, int n) {
+	if(num%n==0) {
+		return true;
+	}
+	return false;
+}
