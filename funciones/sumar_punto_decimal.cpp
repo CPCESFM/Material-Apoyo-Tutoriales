11c54854b867efacd807be47e63a336ce32416ed
Algoritmia Esfm <cpcesfm@gmail.com>
2020-09-15 01:41:28 -0500

    Sumar dos números reales

---
+++ funciones/sumar_punto_decimal.cpp
@@ -0,0 +1,7 @@
+///¿Has escuchado del error de punto flotante?, cuidado ...
+
+long double sumar(long double x, long double y) {
+	long double res=x+y;
+	return res;
+	///Pudimos haber hecho directamente "return x+y;"
+}
